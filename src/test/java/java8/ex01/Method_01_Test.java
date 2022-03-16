8cf75f7448543cb34a27fa7cb20ab81e2f2d6545
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2022-03-16 14:59:23 +0100

    (default-method): TP fini

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -19,6 +19,15 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        public default int sumAge() {
+            int totalAge = 0;
+            for (Person person : findAll()) {
+                totalAge += person.getAge();
+            }
+            return totalAge;
+        }
+
+        ;
     }
     // end::IDao[]
 
@@ -48,7 +57,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +68,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
