de8014ba5e9093627e9c41c8f151636d27671a9c
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2022-03-16 16:26:03 +0100

    (functions): TP fini

--- src/test/java/java8/data/Account.java
+++ src/test/java/java8/data/Account.java
@@ -5,6 +5,11 @@ public class Account {
     private Person owner;
     private Integer balance;
 
+    public Account(Person owner, Integer balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
     public Person getOwner() {
         return owner;
     }
